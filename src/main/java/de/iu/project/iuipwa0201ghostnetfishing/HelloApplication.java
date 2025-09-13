9c7508c54a0339168723b2fff8c132c8e5d4fe05
scherenhaenden <scherenhaenden@gmail.com>
2025-09-13 15:41:17 +0200

    Initial commit: Add Maven wrapper, project structure, and basic REST API with HelloWorld endpoint

---
+++ src/main/java/de/iu/project/iuipwa0201ghostnetfishing/HelloApplication.java
@@ -0,0 +1,9 @@
+package de.iu.project.iuipwa0201ghostnetfishing;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
