0a53bf191e106fa7276200a91f2fbe78d9df2695
scherenhaenden <scherenhaenden@gmail.com>
2025-09-16 19:11:45 +0200

    Implementiere IGhostNetBusinessLayerService-Interface in der GhostNetBusinessLayerService-Klasse

--- src/main/java/de/iu/project/iuipwa0201ghostnetfishing/BusinessLayer/Services/GhostNetBusinessLayerService.java
+++ src/main/java/de/iu/project/iuipwa0201ghostnetfishing/BusinessLayer/Services/GhostNetBusinessLayerService.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-public class GhostNetBusinessLayerService {
+public class GhostNetBusinessLayerService implements IGhostNetBusinessLayerService {
 
     private final GhostNetDataLayerModelRepository repository;
 
