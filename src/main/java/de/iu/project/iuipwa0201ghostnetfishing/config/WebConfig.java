7ae9639eea7d797d567e5823a274c25e72727dcb
scherenhaenden <scherenhaenden@gmail.com>
2025-09-28 14:25:34 +0200

    Aktualisiere WebConfig zur Unterstützung zusätzlicher statischer Ressourcen und passe die Anwendungseigenschaften an, um Konflikte mit Controllern zu vermeiden. Korrigiere den Pfad zur CSS-Datei im Layout-Template.

--- src/main/java/de/iu/project/iuipwa0201ghostnetfishing/config/WebConfig.java
+++ src/main/java/de/iu/project/iuipwa0201ghostnetfishing/config/WebConfig.java
@@ -1,16 +1,32 @@
 package de.iu.project.iuipwa0201ghostnetfishing.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.core.Ordered;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    /*@Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        // Set high precedence for view controllers
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-    }*/
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Configure static resources with lower priority than controllers
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(3600);
+
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/")
+                .setCachePeriod(3600);
+
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/")
+                .setCachePeriod(3600);
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/")
+                .setCachePeriod(3600);
+
+        // Set lower priority for all static resources
+        registry.setOrder(1);
+    }
 }
