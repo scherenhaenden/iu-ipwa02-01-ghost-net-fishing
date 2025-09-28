7df37952dc67ee29b8a39e88f405a186cea519b6
scherenhaenden <scherenhaenden@gmail.com>
2025-09-28 15:24:08 +0200

    Aktualisiere Validierungsanmerkungen in AbandonedNet und GhostNet Modellen. Ersetze @DecimalMin durch @PositiveOrZero und füge @PastOrPresent hinzu. Füge AbandonedNetValidationTest hinzu, um Validierungslogik zu überprüfen.

--- src/main/java/de/iu/project/iuipwa0201ghostnetfishing/DatabaseLayer/Models/GhostNetDataLayerModel.java
+++ src/main/java/de/iu/project/iuipwa0201ghostnetfishing/DatabaseLayer/Models/GhostNetDataLayerModel.java
@@ -2,10 +2,11 @@ package de.iu.project.iuipwa0201ghostnetfishing.DatabaseLayer.Models;
 
 import jakarta.persistence.*;
 import jakarta.persistence.CascadeType;
-import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.PastOrPresent;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -42,7 +43,7 @@ public class GhostNetDataLayerModel implements Serializable {
     /* Area size
        Size of the net in square meters. Not null.
     */
-    @DecimalMin("0.0")
+    @PositiveOrZero
     @Column(name = "SIZE", nullable = false)
     private Double size;
 
@@ -50,6 +51,7 @@ public class GhostNetDataLayerModel implements Serializable {
        Stores when the record was created in the system. Not nullable.
     */
     @Temporal(TemporalType.TIMESTAMP)
+    @PastOrPresent
     @Column(name = "CREATED_AT", nullable = false)
     private Date createdAt = new Date();
 
